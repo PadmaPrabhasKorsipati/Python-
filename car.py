b9189ee91c15c2235eaaad45bb1d108c032487e5
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2026-06-12 23:33:47 +0530

    Auto commit

--- car.py
+++ car.py
@@ -1,7 +1,13 @@
 class car:
-     def __init__(self,model,year,color,for_sale):
+    def __init__(self,model,year,color,for_sale):
           
        self.model =model
        self.year=year
        self.color=color
        self.for_sale=for_sale
+    
+    def drive(self):
+        print(f"You drive the {self.model}")
+
+    def stop(self):
+        print(f"You stop the {self.model}")
