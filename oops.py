a682cf1855c8de3a2f95812274fcf98a4caffe46
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2026-06-12 23:24:20 +0530

    Auto commit

--- oops.py
+++ oops.py
@@ -1,10 +1,4 @@
-class car:
-     def __init__(self,model,year,color,for_sale):
-          
-       self.model =model
-       self.year=year
-       self.color=color
-       self.for_sale=for_sale
+from car import car
 
 
 car1=car("Mustang",2014,"Black",False)
@@ -15,3 +9,7 @@ car3=car("BMW",2023,"Blue",False)
 
 
 
+print(car1.model)
+print(car1.year)
+print(car1.color)
+print(car1.for_sale)
