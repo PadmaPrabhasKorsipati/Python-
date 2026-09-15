9f220a3cdd02e8e0f33a72db34ce97fc55f2a01a
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2026-09-15 21:09:27 +0530

    Auto commit

--- cryptography algorithms/frequency.py
+++ cryptography algorithms/frequency.py
@@ -8,7 +8,7 @@ for ch in c:
     if ch.isalpha():
         freq[ch]=freq.get(ch,0)+1
 
-freq=sorted(freq.items,key=lambda x:x[1],reverse=True)
+freq=sorted(freq.items(),key=lambda x:x[1],reverse=True)
 
 
 for ch,n in freq:
