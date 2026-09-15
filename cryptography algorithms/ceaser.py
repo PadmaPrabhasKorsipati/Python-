fb770ded2d66885fdbe1aff8a96fa552b80f35a9
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2026-09-16 05:18:17 +0530

    Auto commit

--- cryptography algorithms/ceaser.py
+++ cryptography algorithms/ceaser.py
@@ -1,21 +1,22 @@
-c=input("Enter Plain text:")
-key=int(input("Enter key:"))
+c=input("Enter the text:")
+k=int(input("Enter key:"))
+
 e=""
+
 for i in c:
     if i.isalpha():
-        e+=chr((ord(i.upper())-65 +key)%26 +65)
+        e+=chr((ord(i)-65+k)%26+65)
     else:
         e+=i
 
-print(f"Encrypted text:{e}")
 
-p=""
+print(f"Encrypted:"+e)
+
+d=""
+
 for i in e:
     if i.isalpha():
-        p+=chr((ord(i)-65-key)%26 +65)
-
+        d+=chr((ord(i)-65-k)%26+65)
     else:
-        p+=i
-
-print(f"Decrypted text:{p}")
-        
+        d+=i
+print("Decrypted:"+d)
