e21b6b2aa58ae4169930ae2a422bd228b1bc7c75
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2026-09-16 10:15:34 +0530

    Auto commit

--- cryptography algorithms/playfair.py
+++ cryptography algorithms/playfair.py
@@ -1,57 +1,49 @@
-key=input("Enter Key:").upper().replace("J","I")
+k=input("Enter key:").upper().replace("J","I")
 
 p=input("Enter text:").upper().replace("J","I")
 
 s=""
 
-for c in key + "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+for c in k+"ABCDEFGHIJKLMNOPQRSTUVWXYZ":
     if c not in s:
         s+=c
 
 
-m=[s[i:i+5] for i in range(0,25,5)]
+m=[s[i:i+1] for i in range(0,25,5)]
 
-def  pos(c):
-
+def pos(c):
     for i in range(5):
-
-       for j in range(5):
-           if m[i][j]==c:
-               return i,j
-
+        for j in range(5):
+            if m[i][j]==c:
+                return i,j
 
 x=""
 
-i=0;
-
+i=0
 while(i<len(p)):
-
     a=p[i]
-
     b=p[i+1] if i+1<len(p) else "X"
 
     if a==b:
         x+=a+"X"
         i+=1
 
-
     else:
         x+=a+b
 
         i+=2
 
-if len(x)%2:
-
+if len(p)%2:
     x+="X"
 
 
-
 e=""
-
 for i in range(0,len(x),2):
     a=x[i]
     b=x[i+1]
+
     r1,c1=pos(a)
+
     r2,c2=pos(b)
 
     if r1==r2:
@@ -59,14 +51,14 @@ for i in range(0,len(x),2):
 
     elif c1==c2:
 
-        e+=m[(r1+1)%5][c1] + m[(r2+1)%5][c2]
+        e+=m[(r1+1)%5][c1] +m[(r2+1)%5][c2]
 
     else:
-
         e+=m[r1][c2] +m[r2][c1]
 
-print("Encrypted:",e)
 
+
+print("Emncrypted:",e)
 
 
 
