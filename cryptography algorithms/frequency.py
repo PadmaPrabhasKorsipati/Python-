89ee4bffc975c891620173fcd5805433f5f6d4b6
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2026-09-16 06:08:24 +0530

    Auto commit

--- cryptography algorithms/frequency.py
+++ cryptography algorithms/frequency.py
@@ -1,16 +1,18 @@
-c = input("Cipher: ").upper()
+c=input("Enter cipher:").upper()
 
-freq = {}
+
+freq={}
 
 for ch in c:
+
     if ch.isalpha():
-        freq[ch] = freq.get(ch, 0) + 1
+        freq[ch]=freq.get(ch,0)+1
 
-freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+freq=sorted(freq.items(),key=lambda x:x[1],reverse=True)
 
-print("Frequency:")
-for ch, n in freq:
-    print(ch, n)
+print("frequency:")
 
-print("\nMost frequent:", freq[0][0])
-print("Likely plaintext letter: E")
+for ch,n in freq:
+    print(ch,n)
+
+print("\n The most frequent:"+freq[0][0])
