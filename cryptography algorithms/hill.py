b1e40c00cf19c7c21d9df91886b31af2c5048a89
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2026-09-16 06:43:28 +0530

    Auto commit

--- cryptography algorithms/hill.py
+++ cryptography algorithms/hill.py
@@ -1,21 +1,17 @@
-p=input("plain:").upper().replace(" ","")
+p=input("Enter text:").upper().replace(" ","")
 
-k=[[9,4],[5,7]]
-
-if len(p)%2:
-    p+="X"
-
-
+k=[[4,5],[7,9]]
 e=""
-
 for i in range(0,len(p),2):
     x=ord(p[i])-65
+
     y=ord(p[i+1])-65
 
     a=(k[0][0]*x + k[0][1]*y)%26
 
     b=(k[1][0]*x + k[1][1]*y)%26
 
-    e+=chr(a+65) + chr(b+65)
+    e+=chr(a+65)+chr(b+65)
 
-print("Encrypted:",e)
+print("encrypted:"+e)
+
