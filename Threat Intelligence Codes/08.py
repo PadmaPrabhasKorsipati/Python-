1d5c115286511172f2978f9fd9d960176fc43236
PadmaPrabhasKorsipati <prabhas379.my@gmail.com>
2026-09-19 06:04:39 +0530

    Auto commit

---
+++ Threat Intelligence Codes/08.py
@@ -0,0 +1,17 @@
+f=open("scenarios.csv")
+
+next(f)
+
+
+for x in f:
+
+    src,dst,action=x.strip().split(",")
+
+    if action=="block":
+        print(f"{src}->{dst} : BLOCKED")
+
+    else:
+        print(f"{src}->{dst}:ALLOWED")
+
+    
+
